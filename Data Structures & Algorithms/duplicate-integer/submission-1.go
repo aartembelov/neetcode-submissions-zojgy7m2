34ed0f426c1076a942fae6606e1d9feddbfb75b8
Artem <artem.belov.personal@gmail.com>
2026-05-14 17:55:26 +0300

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.go
@@ -0,0 +1,13 @@
+func hasDuplicate(nums []int) bool {
+    hash := map[int]bool{}
+
+    for _, v := range nums {
+        if _, ok := hash[v]; ok {
+            return true
+        }
+
+        hash[v] = true
+    }
+
+    return false
+}
