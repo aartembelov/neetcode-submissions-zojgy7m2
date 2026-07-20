635bd04109cd889905d628e3b1b01054420f0f76
Artem <artem.belov.personal@gmail.com>
2026-07-20 22:29:39 +0400

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.ts
@@ -0,0 +1,39 @@
+// ["1","2","+","3","*","4","-"]
+// 1, stack = [1]
+// 2, stack = [2]
+// +, stack = [3]
+// 3, stack = [3, 3]
+// *, stack = [9]
+// 
+
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens: string[]): number {
+        const operations = {
+            "+": (a: number, b: number) => a + b,
+            "-": (a: number, b: number) => a - b,
+            "*": (a: number, b: number) => a * b,
+            "/": (a: number, b: number) => a / b,
+        }
+
+        const stack: string[] = [];
+        for (let i = 0; i < tokens.length; i++) {
+            if (
+                operations[tokens[i]] && 
+                stack.at(-1) && !operations[stack.at(-1)] &&
+                stack.at(-2) && !operations[stack.at(-2)]
+            ) {
+                const num1 = parseInt(stack.pop());
+                const num2 = parseInt(stack.pop());
+                stack.push(operations[tokens[i]](num2, num1).toString())
+            } else {
+                stack.push(tokens[i]);
+            }
+        }
+        
+        return parseInt(stack.pop());
+    }
+}
